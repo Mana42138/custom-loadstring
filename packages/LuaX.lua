309c85f29fd663e4018af1c1c5cff3da912c6c5c
Mana <86923626+Mana42138@users.noreply.github.com>
2024-08-10 16:22:10 +0200

    Update LuaX.lua

--- packages/LuaX.lua
+++ packages/LuaX.lua
@@ -68,7 +68,25 @@
 --
 --------------------------------------------------------------------]]
 
-local luaZ = require(script.Parent.LuaZ)
+local req = (syn and syn.request) or (http and http.request) or http_request
+
+function GetHttp(URL)
+	local Data = nil
+	local Test = req({
+        Url = URL,
+        Method = 'GET',
+	})
+	for i,v in pairs(Test) do
+		Data = v
+	end
+	return Data
+end
+
+function package(name)
+	return GetHttp("https://raw.githubusercontent.com/Mana42138/custom-loadstring/main/packages/"..name..".lua")
+end
+
+local luaZ = package("LuaZ")
 
 local luaX = {}
 
